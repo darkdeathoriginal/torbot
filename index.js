45e02db5ed1051a8ab5e1aa2faaecc939dc4a5b7
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-06-18 14:43:58 +0530

    chore: Cleanup temporary files

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const { BOT_TOKEN } = require("./config");
+const { BOT_TOKEN,DEVELOPMENT } = require("./config");
 const {Bot} = require("./lib/Bot")
 const commandsPromise = require('./commands');
+const fs = require('fs');
 
 const newBot = new Bot(BOT_TOKEN,"torrent")
-
+//cleanup
+if(!DEVELOPMENT){
+  const dirs = fs.readdirSync('./')
+  for(let dir of dirs){
+    if(dir.startsWith('tor-')){
+      fs.rmSync(dir,{recursive:true})
+    }
+  }
+}
 commandsPromise.then(commands => {
   for(let command of commands){
     newBot.addCommand(command)
