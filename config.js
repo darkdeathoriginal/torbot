f70238161d4a22312bba8803272d2812332eed7a
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-06-18 15:30:31 +0530

    chore: Update config.js to include DATABASE_URL and Sequelize initialization

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
+const { Sequelize } = require('sequelize');
+
+DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
 
 module.exports ={
+    DATABASE_URL: DATABASE_URL,
+    DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: false }), 
     apiId : Number(process.env.API_ID),
     apiHash : process.env.API_HASH,
     BOT_TOKEN:process.env.BOT_TOKEN,
